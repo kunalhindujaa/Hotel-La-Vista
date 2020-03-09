b7b542bd78db2407977897379ea8fdb0a6f5949e
Hinduja <kunal.hinduja@accenture.com>
2020-03-09 15:01:07 +0530

    Image resolution changed & Room Component changed

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+const Room = ({ room }) => {
+  const { name, price, slug, images } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0]} alt={name} />
+        <div className="price-top">
+          <h6> ${price}</h6>
+          <p> per night </p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
+  );
+};
 
-const Room = () => {
-  return <p> Room </p>;
+Room.propTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired
+  })
 };
 
 export default Room;
